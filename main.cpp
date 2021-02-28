6b154edd74cff00ea76cdb8148b81dd3796de8d8
Jaemok C. Lee <WinterCS003@gmail.com>
2021-02-28 09:22:28 -0800

    Testing git on Qt by adding comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/******************************************************************
+ * AUTHORS   : Thomas Benson, David Dang, Jaemok C. Lee, Ellen Yim
+ * PROJECT 1 : Super Warehouse Store
+ * CLASS     : CS 008 Data Structures
+ * SECTION   : MW 6:30p-8:35
+ * DUE DATE  : 04/07/2021
+ ******************************************************************/
 #include "mainwindow.h"
 
 #include <QApplication>
