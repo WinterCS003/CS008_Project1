ab4fe5052ebf822f69195f175b4b3109d47f53c6
WinterCS003 <77065001+WinterCS003@users.noreply.github.com>
2021-03-25 11:02:26 -0700

    changes by Ellen

--- mainwindow.h
+++ mainwindow.h
@@ -31,27 +31,28 @@ private slots:
     void on_quitButton_clicked();       // if user quits, program exits
     void on_loginButton_clicked();      // if user clicks 'login'
 
-
-    void on_dailySales_clicked();
-
     void on_manageMembers_clicked(); // if manage members is clicked
 
-
     void on_makeSale_clicked();
-
+    void on_dailySales_clicked();
     void on_yearlySales_clicked();
+    
+    void on_quitButton_clicked();       // if user quits, program exits
+    void on_manageInventory_clicked();        // if inventory is clicked
 
 private:
     Ui::MainWindow *ui;
     // containers
     Members_Container mc;      // all members
     sales_container all_sales; // all sales
+    inventory iv;
     // update containers
     manageMembers* mM; // add or delete members
     make_sale* ms;     // add sales
     // reports
     daily_sales* dailySale;  // Sales report by day
     yearly_sales* yearlySale; // Sales report by year
+    InventoryTracker* it;
 };
 
 #endif // MAINWINDOW_H
