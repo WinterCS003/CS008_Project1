b5aebe2343cd8346bd2241084093b8dfb7d69173
WinterCS003 <77065001+WinterCS003@users.noreply.github.com>
2021-03-25 10:55:40 -0700

    Changes by Ellen

--- inventorytracker.h
+++ inventorytracker.h
@@ -3,6 +3,9 @@
 
 #include <QDialog>
 #include "inventory.h"
+#include <QTableView>
+#include <QItemDelegate>
+#include <QStandardItemModel>
 
 namespace Ui {
 class InventoryTracker;
@@ -13,20 +16,22 @@ class InventoryTracker : public QDialog
     Q_OBJECT
 
 public:
-    explicit InventoryTracker(QWidget *parent = nullptr);
+    explicit InventoryTracker(QWidget *parent = 0);
     InventoryTracker(QWidget *parent, const inventory& iv);
     ~InventoryTracker();
 
+private slots:
+    void on_exit_clicked();
+
+private:
     void generate_inventory_list();
     void empty();
 
-private slots:
-    void onMainMenu_clicked();
-
-private:
     Ui::InventoryTracker *ui;
-    QString inventory_list;
     inventory list;
+    QTableView itemTable;
+    QStandardItemModel *model;
+    QLineEdit *text;
 };
 
 #endif // INVENTORYTRACKER_H
