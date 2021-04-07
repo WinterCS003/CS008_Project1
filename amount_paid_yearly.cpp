bec8bcee1ec47aec72e0f7723c48e5031bf566b8
Ellen <69826832+ellenyim8@users.noreply.github.com>
2021-04-07 12:23:39 -0700

    Add files via upload

--- amount_paid_yearly.cpp
+++ amount_paid_yearly.cpp
@@ -1,6 +1,14 @@
 #include "amount_paid_yearly.h"
 #include "ui_amount_paid_yearly.h"
 
+/*************************************************************
+ * amount_paid_yearly(QWidget *parent = nullptr);
+ *      CTOR; initialize class objects to null
+ * -----------------------------------------------------------
+ *      Parameters: parent (QWidget*) // IN: ptr to window
+ * -----------------------------------------------------------
+ *      Returns: none
+**************************************************************/
 amount_paid_yearly::amount_paid_yearly(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::amount_paid_yearly)
@@ -8,6 +16,16 @@ amount_paid_yearly::amount_paid_yearly(QWidget *parent) :
     ui->setupUi(this);
 }
 
+/*************************************************************
+ * amount_paid_yearly(QWidget *parent = nullptr,
+ *                    Members_Container* m);
+ *      CTOR; initialize class attributes, hides label/buttons
+ * -----------------------------------------------------------
+ *      Parameters: parent (QWidget*) // IN: ptr to window
+ *                  m (Member_Container*) // IN: ptr to all members
+ * -----------------------------------------------------------
+ *      Returns: none
+**************************************************************/
 amount_paid_yearly::amount_paid_yearly(QWidget *parent, Members_Container* m)
     : QDialog(parent),
       ui(new Ui::amount_paid_yearly)
@@ -22,11 +40,29 @@ amount_paid_yearly::amount_paid_yearly(QWidget *parent, Members_Container* m)
     members = m;
 }
 
+/*************************************************************
+ * ~amount_paid_yearly()
+ *      destructor; frees memory used by ui (window)
+ * -----------------------------------------------------------
+ *      Parameters: none
+ * -----------------------------------------------------------
+ *      Returns: none
+**************************************************************/
 amount_paid_yearly::~amount_paid_yearly()
 {
     delete ui;
 }
 
+/****************************************************************
+ * void on_submit_clicked();
+ *
+ *   Accessor; This method will generate a report of all items
+ *     or a single items based on member type (basic or preferred)
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none - report is generated
+ ***************************************************************/
 void amount_paid_yearly::on_submit_clicked()
 {
     int flag = 2;
@@ -47,9 +83,28 @@ void amount_paid_yearly::on_submit_clicked()
         flag = 2;
     }
 
-    generate_report(flag);
+    if (members->get_members_count() == 0)
+    {
+        QMessageBox::warning(this, "Warning: ", "There are no members. ");
+        return;
+    }
+    else
+    {
+        generate_report(flag);
+    }
+
 }
 
+/****************************************************************
+ * void on_close_clicked();
+ *
+ *   Accessor; This will close window when close (push button)
+ *   is clicked on.
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none (return back to main window)
+ ***************************************************************/
 void amount_paid_yearly::on_close_clicked()
 {
     amount_paid_yearly::close();
@@ -60,67 +115,70 @@ void amount_paid_yearly::generate_report(int flag)
     Members_Container basic;
     Members_Container prefer;
     Members_Container both;
-
-    // check if no members
-    if (members->get_members_count() == 0)
-    {
-        QMessageBox::warning(this, "Warning: ", "There are no members. ");
-        return;
-    }
+    string output = "";
 
     for (int i=0; i < members->get_members_count(); i++) {
         if (flag == 0) {
             basic.add_member(members[i].get_member(i));
         }
-        else if (flag == 1) {
+        if (flag == 1) {
             prefer.add_member(members[i].get_member(i));
         }
-        else {
+        if (flag == 2) {
             both.add_member(members[i].get_member(i));
         }
     }
 
-    for (int i=0; i < basic.get_members_count(); i++) {
-        // Member name
-        report_output += "Member Name: ";
-        report_output += basic.get_member(i).get_name().c_str();
-        report_output += "\n";
+    if (flag == 0) {
+        for (int i=0; i < basic.get_members_count(); i++) {
+            // Member name
+            output += "Member Name: ";
+            output += basic.get_member(i).get_name().c_str();
+            output += "\n";
 
-        // member type :
-        report_output += "Member Type: basic \n";
+            // member type :
+            output += "Member Type: basic \n";
 
-        // member amount total
-        report_output += "Total amount spent per year: ($)";
-        report_output += basic.get_member(i).get_total_spend();
-        report_output += "\n\n";
+            // member amount total
+            output += "Total amount spent per year: ($)";
+            output += basic.get_member(i).get_total_spend();
+            output += "\n\n";
+        }
+
     }
 
-    for (int i=0; i < prefer.get_members_count(); i++) {
-        report_output += "Member name: ";
-        report_output += prefer.get_member(i).get_name().c_str();
-        report_output += "\n";
+    else if (flag == 1) {
+        for (int i=0; i < prefer.get_members_count(); i++) {
+            output += "Member name: ";
+            output += prefer.get_member(i).get_name().c_str();
+            output += "\n";
 
-        report_output += "Member Type: preferred \n";
+            output += "Member Type: preferred \n";
 
-        report_output += "Total amount spent per year: ($)";
-        report_output += prefer.get_member(i).get_total_spend();
-        report_output += "\n\n";
+            output += "Total amount spent per year: ($)";
+            output += prefer.get_member(i).get_total_spend();
+            output += "\n\n";
+        }
+
     }
 
-    for (int i=0; i < both.get_members_count(); i++) {
-        report_output += "Member name: ";
-        report_output += both.get_member(i).get_name().c_str();
-        report_output += "\n";
+    else {
+        for (int i=0; i < both.get_members_count(); i++) {
+            output += "Member name: ";
+            output += both.get_member(i).get_name().c_str();
+            output += "\n";
 
-        report_output += "Member Type: both \n";
+            output += "Member Type: both \n";
 
-        report_output += "Total amount spent per year: ($)";
-        report_output += both.get_member(i).get_total_spend();
-        report_output += "\n\n";
+            output += "Total amount spent per year: ($)";
+            output += both.get_member(i).get_total_spend();
+            output += "\n\n";
+        }
+
     }
-
-    ui->report->show();
+    report_output = QString::fromStdString(output);
     ui->report->setText(report_output);
+    ui->report->show();
 }
 
 
--- amount_paid_yearly.h
+++ amount_paid_yearly.h
@@ -23,17 +23,76 @@ public:
     amount_paid_yearly(QWidget* parent, Members_Container* m);
     ~amount_paid_yearly();
 
+private slots:
+    void on_submit_clicked();
+    void on_close_clicked();
+
+private:
     // displays each members amount paid per year with their name and member type
     void generate_report(int flag);
 
-private slots:
-    void on_submit_clicked();
-    void on_close_clicked();    // if 'close' button is pushed- exit window
-
-private:
     Ui::amount_paid_yearly *ui;
     Members_Container *members;
     QString report_output;
 };
+
+
+/*******************************
+ ** CONSTRUCTORS & DESTRUCTOR **
+ *******************************/
+
+/*************************************************************
+ * amount_paid_yearly(QWidget *parent = nullptr);
+ *      CTOR; initialize class objects to null
+ * -----------------------------------------------------------
+ *      Parameters: QWidget*
+ * -----------------------------------------------------------
+ *      Returns: none
+**************************************************************/
+
+/*************************************************************
+ * amount_paid_yearly(QWidget* parent, Members_Container* m);
+ *      CTOR; initialize class attributes;
+ * -----------------------------------------------------------
+ *      Parameters: parent (QWidget*) // IN: ptr to window
+ *          m (Members_Container*)  // IN: ptr to all members
+ * -----------------------------------------------------------
+ *      Returns: none
+**************************************************************/
+
+/*************************************************************
+ * ~amount_paid_yearly()
+ *      Destructor; frees memory used by ui (window)
+ * -----------------------------------------------------------
+ *      Parameters: none
+ * -----------------------------------------------------------
+ *      Returns: none
+**************************************************************/
+
+
+/***************
+ ** ACCESSORS **
+ **************/
+
+/*************************************************************
+ * void on_submit_clicked()
+ *      Accessor; checks if basic / preferred checkbox is
+ * checked, and sets to an int and then calls generate_report(int)
+ * -----------------------------------------------------------
+ *      Parameters: none
+ * -----------------------------------------------------------
+ *      Returns: none
+**************************************************************/
+
+
+/*************************************************************
+ * void on_close_clicked()
+ *      Accessor; closes the amount_paid_yearly window
+ * ----------------------------------------------------------
+ *      Parameters: none
+ * ----------------------------------------------------------
+ *      Returns: none
+**************************************************************/
+
 
 #endif // AMOUNT_PAID_YEARLY_H
