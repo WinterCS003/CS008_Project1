8cfcfef8092fde44ffd402de027f80633f649546
WinterCS003 <77065001+WinterCS003@users.noreply.github.com>
2021-04-07 11:54:29 -0700

    Update item_reports.cpp

--- item_reports.cpp
+++ item_reports.cpp
@@ -3,6 +3,13 @@
 
 #include <algorithm>
 
+/****************************************************************
+ * item_reports(QWidget* parent = nullptr);
+ *   Constructor; Initialize class attributes to null
+ *   Parameters: parent (QWidget*) // IN - pointer to window
+ *   Return: none
+ ***************************************************************/
+
 item_reports::item_reports(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::item_reports)
@@ -10,28 +17,58 @@ item_reports::item_reports(QWidget *parent) :
     ui->setupUi(this);
 }
 
-item_reports::item_reports(QWidget *parent,
-                           sales_container* all_sales,
-                           inventory* all_items):
+/****************************************************************
+ * item_reports(QWidget *parent,
+                 sales_container* all_sales,
+                 inventory* all_items);
+ *   Constructor; Initialize class attributes
+ *   Parameters: parent (QWidget*) // IN - pointer to window
+                 all_sales (sales_container*) // IN - pointer to
+                                              // all sales made
+                 all_items (inventory*) // IN - pointer to all
+                                        // items in stock
+ *   Return: none
+ ***************************************************************/
+
+item_reports::item_reports(QWidget *parent, // IN - pointer to window
+                           sales_container* all_sales, // IN - pointer to
+                                                       // all sales made
+                           inventory* all_items): // IN - pointer to all
+                                                  // items in stock
     QWidget(parent),
     ui(new Ui::item_reports),
     all_sales(all_sales),
     all_items(all_items)
 {
     ui->setupUi(this);
-
-    ui->report->hide();
-    ui->back->hide();
 }
+
+/****************************************************************
+ * ~item_reports();
+ *   Destructor; Frees memory used by the window
+ *   Parameters: none
+ *   Return: none
+ ***************************************************************/
 
 item_reports::~item_reports()
 {
     delete ui;
 }
 
+/****************************************************************
+ * void on_submitButton_clicked();
+ *
+ *   Accessor; This method will generate a report of all items
+ *     or a single items based on user input.
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none - report is generated
+ ***************************************************************/
+
 void item_reports::on_submitButton_clicked()
 {
-    switchScreen();
+    ui->report->clear();
     if(ui->allItems->isChecked())
     {
         allItemReport();
@@ -50,8 +87,22 @@ void item_reports::on_submitButton_clicked()
     }
 }
 
-void item_reports::singleItemReport(std::string itemName)
+/****************************************************************
+ *  void singleItemReport(std::string itemName);
+ *
+ *   Accessor; This method will generate a report on a single
+ *     item, sold and in stock based off of an itemName.
+ * --------------------------------------------------------------
+ *   Parameters: itemName (std::string) // IN - name of item to
+ *                                      //      report
+ * --------------------------------------------------------------
+ *   Return: none - report on single item is generated
+ ***************************************************************/
+
+void item_reports::singleItemReport(std::string itemName) // IN - name of item to
+                                                          //      report
 {
+    ui->report->clear();
     sales_container output;
     for(size_t i = 0; i < all_sales->size(); i++)
     {
@@ -79,22 +130,43 @@ void item_reports::singleItemReport(std::string itemName)
     report += "Item Name: ";
     report += itemName.c_str();
     report += "\n";
-    report += "Item Quantity: ";
+    report += "Item Quantity sold: ";
     report += to_string(quantity).c_str();
-    report += "\n\n";
-    report += "Item price: ";
-    report += to_string(output[0].getPrice()).c_str();
     report += "\n";
-    report += "Total price: ";
-    report += to_string(price).c_str();
+    report += "Item Quantity in stock: ";
+    report += to_string((*all_items)[all_items->search(itemName)].get_quantity()).c_str();
+    report += "\n\n";
+    report += "Item price: $";
+    double revenue = std::ceil(output[0].getPrice()*100.0)/100.0;
+    std::string rev = to_string(revenue);
+    rev = rev.substr(0, rev.find(".")+3);
+    report += QString::fromStdString(rev);
+    report += "\n";
+    report += "Total revenue: $";
+    revenue = std::ceil(price*100.0)/100.0;
+    rev = to_string(revenue);
+    rev = rev.substr(0, rev.find(".")+3);
+    report += QString::fromStdString(rev);
     report += "\n\n";
     report += "-------------End Report-------------";
 
     ui->report->setText(report);
 }
 
+/****************************************************************
+ * void allItemReport();
+ *
+ *   Accessor; This method will generate a report on all items
+ *     in the all_items container.
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none - report is generated
+ ***************************************************************/
+
 void item_reports::allItemReport()
 {
+    ui->report->clear();
     // quantity sorted by name
     sales_container unique_sales;
     for(size_t i = 0; i < all_sales->size(); i++)
@@ -115,7 +187,6 @@ void item_reports::allItemReport()
     if(unique_sales.size() == 0)
     {
         QMessageBox::warning(this, "Warning", "No sales made for item");
-        switchScreen();
         return;
     }
 
@@ -125,45 +196,26 @@ void item_reports::allItemReport()
     });
 
     QString output = "---------------Report--------------\n\n";
-    for(size_t i = 0; i < unique_sales.size(); i++)
+    int sold = 0;
+    for(size_t i = 0; i < all_items->size(); i++)
     {
+        int index = unique_sales.find((*all_items)[i].get_item_name());
+        index != -1 ? sold = unique_sales[index].getQuantity() : sold = 0;
+
         output += "Item name: ";
-        output += unique_sales[i].getItem().c_str();
-        output += "Item quantity: ";
-        output += to_string(unique_sales[i].getQuantity()).c_str();
+        output += QString::fromStdString((*all_items)[i].get_item_name());
         output += "\n";
+        output += "Item quantity sold: ";
+        output += QString::fromStdString(to_string(sold));
+        output += "\n";
+        output += "Item quantity in Stock: ";
+        output += QString::fromStdString(to_string((*all_items)[i].get_quantity()));
+        output += "\n\n";
     }
-    output += "\n\n------------End Report------------";
+    output += "Total Number of Items: ";
+    output += QString::fromStdString(to_string((*all_items).size()));
+    output += "\n\n";
+    output += "------------End Report------------";
 
     ui->report->setText(output);
-}
-
-void item_reports::switchScreen()
-{
-    // input screen
-    if(ui->report->isHidden())
-    {
-        ui->report->clear();
-        ui->nameInput->hide();
-        ui->nameLabel->hide();
-        ui->allItems->hide();
-        ui->submitButton->hide();
-        ui->report->show();
-        ui->back->show();
-    }
-    else
-    {
-        ui->nameInput->clear();
-        ui->nameInput->show();
-        ui->nameLabel->show();
-        ui->allItems->show();
-        ui->submitButton->show();
-        ui->report->hide();
-        ui->back->hide();
-    }
-}
-
-void item_reports::on_back_clicked()
-{
-    switchScreen();
 }
