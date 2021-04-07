297c22136fffc991ffd205c5a4624ba59fd65f34
WinterCS003 <77065001+WinterCS003@users.noreply.github.com>
2021-04-07 11:58:24 -0700

    Update memberpurchase.h

--- memberpurchase.h
+++ memberpurchase.h
@@ -25,20 +25,107 @@ public:
     ~memberPurchase();
 
 private slots:
-    void on_back_clicked();
-
+    /***************
+     ** ACCESSORS **
+     ***************/
     void on_submit_clicked();
 
 private:
-    void singleMemberReport(unsigned int id);
-    void singleMemberReport(std::string name);
     void allMemberReport();
-    void switchScreen();
 
     Ui::memberPurchase *ui;
     sales_container* all_sales;
-    inventory* all_items;
     Members_Container* all_members;
+    inventory* all_items;
 };
 
 #endif // MEMBERPURCHASE_H
+
+/****************************************************************
+ * memberPurchase Class
+ *   This class represents a member purchases window, it manages
+ *     4 attributes, a pointer to the ui window, a pointer
+ *     to a container of all sales made, a pointer to all members
+ *     and a pointer to all items in the inventory.
+ ***************************************************************/
+
+
+/******************************
+ ** CONSTRUCTOR & DESTRUCTOR **
+ ******************************/
+
+/****************************************************************
+ * memberPurchase(QWidget *parent = nullptr);
+ *   Constructor; Null initializes class attributes
+ *   Parameters: parent (QWidget*) // IN - pointer to window
+ *   Return: none
+ ***************************************************************/
+
+/****************************************************************
+ * memberPurchase(QWidget* parent,
+                   sales_container* sc,
+                   Members_Container* mc,
+                   inventory* iv);
+ *   Constructor; Initialize class attributes
+ *   Parameters: parent (QWidget*) // IN  - pointer to window
+                   sc (sales_container*) // IN - pointer to all sales made
+                   mc (Members_Container*) // IN - ponter to all members
+                   iv (inventory*) // IN - pointer to all items in stock
+ *   Return: none
+ ***************************************************************/
+
+/****************************************************************
+ * ~memberPurchase();
+ *   Destructor; Frees memory used by the window
+ *   Parameters: none
+ *   Return: none
+ ***************************************************************/
+
+
+/***************
+  ** ACCESSORS **
+  ***************/
+
+/****************************************************************
+ * void on_back_clicked();
+ *
+ *   Accessor; This method will call the switchScreen method
+ *     and allows the user to toggle between input and report
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none - screen is switched
+ ***************************************************************/
+
+/****************************************************************
+ * void on_submit_clicked();
+ *
+ *   Accessor; This method will submit all user input and generate
+ *     a report based on the input.
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none - report is generated
+ ***************************************************************/
+
+/****************************************************************
+ * void allMemberReport();
+ *
+ *   Accessor; This method will generate a report of all
+ *     sales made by every member in the all_members container.
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none - report is generated for all members
+ ***************************************************************/
+
+/****************************************************************
+ * void switchScreen();
+ *
+ *   Accessor; This method will allow the user to toggle between
+ *     input and report screens.
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none - screen is switched
+ ***************************************************************/
