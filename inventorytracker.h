cbd018e7f2db65f52bf669dcb5bad855ca94c706
WinterCS003 <77065001+WinterCS003@users.noreply.github.com>
2021-04-07 16:50:03 -0700

    Update inventorytracker.h

--- inventorytracker.h
+++ inventorytracker.h
@@ -22,6 +22,9 @@ public:
     ~InventoryTracker();
 
 private slots:
+    /***************
+     ** ACCESSORS **
+     ***************/
     void on_exit_clicked();
 
 private:
@@ -37,3 +40,78 @@ private:
 };
 
 #endif // INVENTORYTRACKER_H
+
+/****************************************************************
+ * InventoryTracker Class
+ *   This class represents an inventory tracker widget, it
+ *   generates a report based off of all the items in stock
+ *   and that were sold. It handles a sales_container, QTableView,
+ *   QStandardItemModel, QLineEdit, and inventory pointers.
+ ***************************************************************/
+
+
+/******************************
+ ** CONSTRUCTOR & DESTRUCTOR **
+ ******************************/
+
+/****************************************************************
+ * InventoryTracker(QWidget *parent = 0);
+ *   Constructor; Initialize class attributes to null
+ *   Parameters: parent (QWidget*) // IN - pointer to widget
+ *   Return: none
+ ***************************************************************/
+
+/****************************************************************
+ * InventoryTracker(QWidget *parent,
+ *                  inventory* iv,
+ *                  sales_container* sales);
+ *   Constructor; Initialize class attributes to parameters
+ *   Parameters: QWidget *parent, // IN - pointer to widget window
+ *               inventory* iv, // IN - inventory pointer
+ *               sales_container* sales); // IN - sales container pointer
+ *   Return: none
+ ***************************************************************/
+
+/****************************************************************
+ * ~InventoryTracker();
+ *   Destructor; Deletes ui pointer
+ *   Parameters: none
+ *   Return: none
+ ***************************************************************/
+
+
+/***************
+  ** ACCESSORS **
+  ***************/
+
+/****************************************************************
+ * void on_exit_clicked();
+ *
+ *   Accessor; This method will close the window
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none
+ ***************************************************************/
+
+/****************************************************************
+ void generate_inventory_list();
+ *
+ *   Accessor; This method will create a report of all items
+ *   in stock and how many were sold
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none - report is printed
+ ***************************************************************/
+
+/****************************************************************
+ * void empty();
+ *
+ *   Accessor; This method will notify the user if the
+ *   inventory is empty
+ * --------------------------------------------------------------
+ *   Parameters: none
+ * --------------------------------------------------------------
+ *   Return: none
+ ***************************************************************/
